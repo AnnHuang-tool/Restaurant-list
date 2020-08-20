e532076b1f8adbdcad8188ea427ab0a8dc57100b
Ann Huang <s52561@yahoo.com.tw>
2020-08-21 02:17:38 +0800

    feat: add message partials

--- restaurant_list/app.js
+++ restaurant_list/app.js
@@ -44,6 +44,7 @@ const bodyParser = require('body-parser')
 // 載入 method-override
 const methodOverride = require('method-override')
 
+const flash = require('connect-flash')   // 引用套件
 
 
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
@@ -70,11 +71,15 @@ app.use(methodOverride('_method'))
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
 
+app.use(flash())  // 掛載套件
+
 
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 
