77dfe7a5bf5e1c8de51b796fed5da24b206ccd29
Ann Huang <s52561@yahoo.com.tw>
2020-08-23 20:30:50 +0800

    feat:add seeder

--- restaurant_list/models/seeds/resSeeder.js
+++ restaurant_list/models/seeds/resSeeder.js
@@ -2,34 +2,83 @@ const bcrypt = require('bcryptjs')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-const Todo = require('../todo')
+const Restaurant = require('../restaurant')
 const User = require('../user')
 const db = require('../../config/mongoose')
-const SEED_USER = {
-  name: 'root',
-  email: 'root@example.com',
-  password: '12345678'
-}
+const restaurantsList = require('../../restaurant.json');
+const SEED_USER = [
+  {
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678'
+  },
+  {
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678'
+  }
+]
+
 db.once('open', () => {
-  bcrypt
-    .genSalt(10)
-    .then(salt => bcrypt.hash(SEED_USER.password, salt))
-    .then(hash => User.create({
-      name: SEED_USER.name,
-      email: SEED_USER.email,
-      password: hash
-    }))
-    .then(user => {
-      const userId = user._id
-      for (let i = 0; i < 10; i++) {
-        Todo.create({ name: `name-${i}`, userId })
-      }
-    })
-    .then(() => {
-      console.log('done.')
-      process.exit()
-    })
+  Promise.all(Array.from(
+    { length: 2 },
+    (_, i) =>
+      bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(SEED_USER[i].password, salt))
+        .then(hash => User.create({
+          name: SEED_USER[i].name,
+          email: SEED_USER[i].email,
+          password: hash
+        }))
+        .then(user => {
+          const userId = { userId: user._id }
+          let restaurantOwner = []
+          restaurantList.results.forEach(data => {
+            if (user.name === 'User1' && data.id <= 3) {
+              data = Object.assign(data, userId)
+              restaurantOwner.push(data)
+            }
+            else if (user.name === 'User2' && data.id > 3) {
+              data = Object.assign(data, userId)
+              restaurantOwner.push(data)
+            }
+          })
+          return Promise.all(Array.from(
+            { length: 3 }, (_, i) => Restaurant.create(restaurantOwner[i]))
+          )
+        })
+        .then(() => {
+          console.log('done.')
+          process.exit()
+        })
+  ))
 })
+
+
+
+
+
+// db.once('open', () => {
+//   bcrypt
+//     .genSalt(10)
+//     .then(salt => bcrypt.hash(SEED_USER[i].password, salt))
+//     .then(hash => User.create({
+//       name: SEED_USER[i].name,
+//       email: SEED_USER[i].email,
+//       password: hash
+//     }))
+//     .then(user => {
+//       const userId = user._id
+//       for (let i = 0; i < 10; i++) {
+//         Todo.create({ name: `name-${i}`, userId })
+//       }
+//     })
+//     .then(() => {
+//       console.log('done.')
+//       process.exit()
+//     })
+// })
 
 
 
